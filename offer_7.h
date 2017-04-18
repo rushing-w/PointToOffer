dfaca40e08afd81b7c024100f836893b5b1786c5
rushing-w <1030104052@qq.com>
2017-04-18 11:13:23 +0800

    wc:剑指offer第11题-->求数值的整数次方

---
+++ offer_7.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include<iostream>
+using namespace std;
+#include<stack>
+
+//两个栈实现一个队列的插入删除
+template<class T>
+class Queue
+{
+public:
+	Queue()
+	{}
+
+	~Queue()
+	{}
+
+	void Push(const T& x)
+	{
+		s1.push(x);
+	}
+
+	T Pop()
+	{
+		if (s2.size() <= 0)
+		{
+			while (s1.size() > 0)
+			{
+				T& data = s1.top();
+				s1.pop();
+				s2.push(data);
+			}
+		}
+
+		if (s2.size() == 0)
+			cout << "queue is empty" << endl;
+
+		T head = s2.top();
+		s2.pop();
+		return head;
+	}
+
+private:
+	stack<T> s1;
+	stack<T> s2;
+};
